973a94d8a4a1bd9b47f195573b5e3938b56b1dee
mixelpixel <mixelpix@mac.com>
2017-06-30 13:36:55 -0400

    slugged in counter code

--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.incrementCount = this.incrementCount.bind(this);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount() {
+    this.setState(currentState => {
+      return {
+        count: currentState.count + 1
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Clock</h1>
+        {this.state.count}
+        <button onClick={this.incrementCount}>Increment</button>
       </div>
     );
   }
